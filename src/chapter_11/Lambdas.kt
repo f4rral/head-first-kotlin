5c7b5557a3018fc7de9b21a34af92638f7a52bdf
f4rral <f4rral@gmail.com>
2024-09-08 19:48:55 +0300

    chapter_11

--- src/chapter_11/Lambdas.kt
+++ src/chapter_11/Lambdas.kt
@@ -1,7 +1,9 @@
 package chapter_11
 
+typealias DoubleConversion = (Double) -> Double
+
 fun convert(x: Double,
-            converter: (Double) -> Double) : Double {
+            converter: DoubleConversion) : Double {
     val result = converter(x)
     println("$x is converted to $result")
 
@@ -13,6 +15,22 @@ fun convertFive(converter: (Int) -> Double): Double {
     println("5 is converted to $result")
 
     return result
+}
+
+fun getConversionLambda(srt: String) : DoubleConversion {
+    if (srt == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (srt == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (srt == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(lambda1: DoubleConversion, lambda2: DoubleConversion): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
 }
 
 fun main(args: Array<String>) {
@@ -36,4 +54,19 @@ fun main(args: Array<String>) {
     convert(20.0, { it * 1.8 + 32 })
 
     convertFive { it * 1.8 + 32 }
+
+    // Преобразовать 2.5 кг в фунты
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+
+    // Определить два лямбда-выражения для преобразований
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    // Два лямбда-выражения преобразуются в одно новое
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    // Использовать новое лямбда-выражение для преобразования
+    // 17,4 кг в американские тонны
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 }
